e6ec060b5c1a556685d6f6d214716e58f91c87af
Kevin Charles <kevinismage@gmail.com>
2020-11-14 21:39:27 +0700

    Auth State & Private Routes

--- todomanager/frontend/src/actions/types.js
+++ todomanager/frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const DELETE_TODO = "DELETE_TODO";
 export const ADD_TODO = "ADD_TODO";
 export const GET_ERRORS = "GET_ERRORS";
 export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
